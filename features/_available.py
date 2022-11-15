210c6a6470c0844da12f306b8fa620c5bbbf99ca
luminosity <agerio100@naver.com>
2022-11-15 12:00:01 +0900

    changed prefix to axl!
    to solve duplicated prefix problem with Mee6 that is very popular in Discord, changed prefix from `!` to `axl! `. Now commands will be activated with `axl! [commands]`

--- features/_available.py
+++ features/_available.py
@@ -1,27 +1,27 @@
 supported_commands = {
     "!change_status"  : {
         "description" : "봇의 상태를 바꿉니다",
-        "usage"       : "!change_status [online|offline|idle|do_not_disturb|invisible]",
+        "usage"       : "axl! change_status [online|offline|idle|do_not_disturb|invisible]",
         "privilege"   : "administrator"
     },
     "!time"  : {
         "description" : "현재 시간을 초 단위로 알려줍니다. 단, 시간은 서버가 위치한 한국시(한국표준시, KST)를 기준으로 합니다.",
-        "usage"       : "!time",
+        "usage"       : "axl! time",
         "privilege"   : "everyone"
     },
     "!hello"          : {
         "description" : "봇에게 인사를 시킵니다.",
-        "usage"       : "!hello",
+        "usage"       : "axl! hello",
         "privilege"   : "everyone"
     },
     "!help"           : {
         "description" : "도움말을 출력합니다.",
-        "usage"       : "!help [command(without \"!\")]",
+        "usage"       : "axl! help [command(without \"!\")]",
         "privilege"   : "everyone"
     },
     # "!ping"           : {
     #     "description" : "네트워크 연결 상태를 확인합니다.",
-    #     "usage"       : "!ping [target_ip|target_address]",
+    #     "usage"       : "axl! ping [target_ip|target_address]",
     #     "privilege"   : "everone"
     # },
 }
--- run.py
+++ run.py
@@ -9,7 +9,7 @@ TOKEN_FILE = open("./asset/token/token.txt", 'r')
 TOKEN      = TOKEN_FILE.readline()
 TOKEN_FILE.close()
 
-bot = commands.Bot(command_prefix = '!', help_command=None, intents=discord.Intents.all())
+bot = commands.Bot(command_prefix = 'axl! ', help_command=None, intents=discord.Intents.all())
 
 ##########################################################
 
@@ -55,7 +55,8 @@ async def help(ctx, *args):
             available_commands_list += f"`{_dataline}` "
         embed = discord.Embed(title = "명령어 도움말", color = 0xE4F9F5)
         embed.add_field(name = "사용 가능한 명령어", value = f"{available_commands_list}", inline = False)
-        embed.add_field(name = "특정 명령어 자세하게 알아보기", value = f"`!help [command]`\n(`[command]`에서 명령어 앞의 !는 붙이지 마세요.)", inline = False)
+        embed.add_field(name = "공통 명령어 형식(prefix)", value = f"`axl! [command] [args...]`", inline = False)
+        embed.add_field(name = "특정 명령어 자세하게 알아보기", value = f"`!help [command]`\n(`[command]`에서 명령어 앞의 axl!는 붙이지 마세요.)", inline = False)
         await ctx.reply(embed = embed)
     elif len(args) == 1:
         request = "!" + args[0]
@@ -71,7 +72,7 @@ async def help(ctx, *args):
             embed.set_footer(text = "명령어 권한이 administrator인 경우 권한이 있는 관리자만 실행할 수 있어요.")
             await ctx.reply(embed = embed)
     else:
-        await ctx.reply(f"명령어 형식이 올바르지 않은 것 같아요. (`!help`)")
+        await ctx.reply(f"명령어 형식이 올바르지 않은 것 같아요. (`axl! help [args...]`)")
 
 
 @bot.event
