dfba620a8daf217e44fcbe8291f3326cbd3d3ac2
luminosity <agerio100@naver.com>
2022-11-22 23:13:26 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,10 @@ LOGFILEPATH="./log/aexol_log_$TIMESTAMP.log"
 if [ ! -d log ];
 then
     mkdir ./log/
+    echo "No /log/ directory for log file storage found. Created it automatically."
 fi
 
+echo "Log file name for this execution : $LOGFILEPATH"
+
 nohup python3 ./bot.py 1>"$LOGFILEPATH" 2>&1 &
+echo "program executed"
